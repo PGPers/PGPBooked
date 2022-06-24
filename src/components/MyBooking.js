74b8fc75cddee566fbc97133a3c44046cf29d0d1
stevenantya <e0851459@u.nus.edu>
2022-06-24 12:10:07 +0700

    Booking Table, New booking start end date time

--- src/components/MyBooking.js
+++ src/components/MyBooking.js
@@ -3,32 +3,91 @@ import { firebase } from "../firebase-config";
 import {
   HStack,
   Button,
+  TableContainer,
+  Table,
+  TableCaption,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
 } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const MyBooking = () => {
   const uid = firebase.auth().currentUser.uid;
-  const viewBooking = async () => {
-    const bookings = [];
-    const bookRef = firebase.firestore().collection(`users/${uid}/bookings`);
-    const bookSnap = await bookRef.get();
-    bookSnap.forEach(doc => {
-      console.log(doc.id, doc.data());
-      bookings.push(doc.data());
-    })
-    console.log(bookings);
-  }
+  const [isBusy, setBusy] = useState(true);
+  const [bookings, setBookings] = useState();
+  // const bookings = [
+  //   {
+  //     facility: "test",
+  //   },
+  //   {
+  //     facility: "test2",
+  //   },
+  // ];
+
+  useEffect(() => {
+    async function fetchData() {
+      const dummyBookings = [];
+      const bookRef = firebase.firestore().collection(`users/${uid}/bookings`);
+      const bookSnap = await bookRef.get();
+      bookSnap.forEach((doc) => {
+        console.log(doc.id, doc.data());
+        dummyBookings.push(doc.data());
+      });
+      setBookings(dummyBookings);
+      setBusy(false);
+      console.log(bookings);
+    }
+    fetchData();
+  }, []);
+
+  const convertDate = (date) => {
+    var t = new Date(1970, 0, 1); // Epoch
+    t.setSeconds(date.seconds);
+    return t.toLocaleString();
+  };
+
   return (
-    <HStack alignSelf={"end"}>
-    <Button
-      onClick={viewBooking}
-      rounded="none"
-      colorScheme="blue"
-      w={["full", "auto"]}
-      alignSelf="end"
-    >
-      View
-    </Button>
-  </HStack>
+    <div>
+      <TableContainer>
+        <Table variant="simple" textAlign={"center"}>
+          <TableCaption>My PGP Bookings</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Booking ID</Th>
+              <Th>Matric</Th>
+              <Th>Facility</Th>
+              <Th>Purpose</Th>
+              <Th>No. of People</Th>
+              <Th>Date Booked</Th>
+              <Th>Start Time</Th>
+              <Th>End Time</Th>
+              <Th>Booking Status</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {bookings &&
+              bookings.map((item) => {
+                return (
+                  <tr>
+                    <td>{item.bookid}</td>
+                    <td>{item.matric} </td>
+                    <td>{item.facility}</td>
+                    <td>{item.purpose}</td>
+                    <td>{item.numOfPeople}</td>
+                    <td>{convertDate(item.date)}</td>
+                    <td>0700</td>
+                    <td>0900</td>
+                    <td>CONFIRMED</td>
+                  </tr>
+                );
+              })}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
--- src/components/NewBooking.js
+++ src/components/NewBooking.js
@@ -18,6 +18,7 @@ import {
   NumberIncrementStepper,
   NumberDecrementStepper,
 } from "@chakra-ui/react";
+import { DatePicker } from "chakra-ui-date-input";
 
 const NewBooking = () => {
   const [facility, setFacility] = useState("");
@@ -29,7 +30,7 @@ const NewBooking = () => {
 
   const makeBooking = async () => {
     await AddBooking(uid, facility, date, matric, numOfPeople, purpose);
-  }
+  };
 
   return (
     <Box
@@ -46,31 +47,120 @@ const NewBooking = () => {
           <Heading>Make a Booking</Heading>
           <Text>Enter your booking details</Text>
         </VStack>
-        <FormControl isRequired>
-          <FormLabel htmlFor='facility'>Choose a Facility</FormLabel>
-          <Select id='facility' placeholder='Select facility' onChange={(e) => {setFacility(e.target.value)}}>
-            <option>Badminton Court</option>
-            <option>Music Room</option>
-          </Select>
-        </FormControl>
-        <FormControl isRequired>
-          <FormLabel htmlFor='matric'>Matric No</FormLabel>
-          <Input id='matric' placeholder='Matric No' onChange={(e) => {setMatric(e.target.value)}}/>
-        </FormControl>
-        <FormControl isRequired>
-          <FormLabel htmlFor='purpose'>Purpose</FormLabel>
-          <Input id='purpose' placeholder='Purpose' onChange={(e) => {setPurpose(e.target.value)}}/>
-        </FormControl>
-        <FormControl isRequired>
-        <FormLabel htmlFor='amount'>Number of People</FormLabel>
-        <NumberInput max={50} min={1} onChange={(e) => {setNumOfPeople(e)}}>
-          <NumberInputField id='amount' />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-        </FormControl>
+        <HStack>
+          <VStack>
+            <FormControl isRequired>
+              <FormLabel htmlFor="facility">Choose a Facility</FormLabel>
+              <Select
+                id="facility"
+                placeholder="Select facility"
+                onChange={(e) => {
+                  setFacility(e.target.value);
+                }}
+              >
+                <option>Badminton Court</option>
+                <option>Music Room</option>
+              </Select>
+            </FormControl>
+            {/* <FormControl isRequired>
+              <FormLabel htmlFor="matric">Matric No</FormLabel>
+              <Input
+                id="matric"
+                placeholder="Matric No"
+                onChange={(e) => {
+                  setMatric(e.target.value);
+                }}
+              />
+            </FormControl> */}
+            <FormControl isRequired>
+              <FormLabel htmlFor="purpose">Purpose</FormLabel>
+              <Input
+                id="purpose"
+                placeholder="Purpose"
+                onChange={(e) => {
+                  setPurpose(e.target.value);
+                }}
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel htmlFor="amount">Number of People</FormLabel>
+              <NumberInput
+                max={50}
+                min={1}
+                onChange={(e) => {
+                  setNumOfPeople(e);
+                }}
+              >
+                <NumberInputField id="amount" />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+          </VStack>
+          <VStack>
+            <FormControl isRequired>
+              <FormLabel htmlFor="startTime">Date</FormLabel>
+              <DatePicker
+                placeholder="Date"
+                name="date"
+                onChange={(date: string) => console.log(date)}
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel htmlFor="startTime">Start Time</FormLabel>
+              <Select
+                id="startTime"
+                placeholder="Start Time"
+                // onChange={(e) => {
+                //   setFacility(e.target.value);
+                // }}
+              >
+                <option>0900</option>
+                <option>1000</option>
+                <option>1100</option>
+                <option>1200</option>
+                <option>1300</option>
+                <option>1400</option>
+                <option>1500</option>
+                <option>1600</option>
+                <option>1700</option>
+                <option>1800</option>
+                <option>1900</option>
+                <option>2000</option>
+                <option>2100</option>
+                <option>2200</option>
+              </Select>
+            </FormControl>
+
+            <FormControl isRequired>
+              <FormLabel htmlFor="endTime">End Time</FormLabel>
+              <Select
+                id="endTime"
+                placeholder="End Time"
+                // onChange={(e) => {
+                //   setFacility(e.target.value);
+                // }}
+              >
+                <option>0900</option>
+                <option>1000</option>
+                <option>1100</option>
+                <option>1200</option>
+                <option>1300</option>
+                <option>1400</option>
+                <option>1500</option>
+                <option>1600</option>
+                <option>1700</option>
+                <option>1800</option>
+                <option>1900</option>
+                <option>2000</option>
+                <option>2100</option>
+                <option>2200</option>
+              </Select>
+            </FormControl>
+          </VStack>
+        </HStack>
         <HStack alignSelf={"end"}>
           <Button
             onClick={makeBooking}
